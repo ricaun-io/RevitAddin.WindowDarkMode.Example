56797bc3d9c48fd22bf0c73d09588a8b697112ae
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-02-05 19:42:17 -0300

    Add `SetCaptionColor`

--- RevitAddin.WindowDarkMode.Example/Views/DesktopWindowManager.cs
+++ RevitAddin.WindowDarkMode.Example/Views/DesktopWindowManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Drawing;
 using System.Windows;
 
 namespace RevitAddin.WindowDarkMode.Example.Views
@@ -28,7 +29,7 @@ namespace RevitAddin.WindowDarkMode.Example.Views
             if (IsImmersiveDarkModeSupported == false) 
                 return false;
 
-            int darkMode = enable ? 1 : 0;
+            var darkMode = enable ? 1u : 0;
             var result = DwmSetWindowAttribute(hWnd, DWMWA_USE_IMMERSIVE_DARK_MODE, ref darkMode, sizeof(int));
             return result > 0;
         }
@@ -43,11 +44,79 @@ namespace RevitAddin.WindowDarkMode.Example.Views
         /// <param name="cbAttribute"></param>
         /// <returns></returns>
         [DllImport("dwmapi.dll", SetLastError = false, ExactSpelling = true)]
-        static extern int DwmSetWindowAttribute(IntPtr hwnd, uint dwAttribute, ref int pvAttribute, int cbAttribute);
+        static extern int DwmSetWindowAttribute(IntPtr hwnd, uint dwAttribute, ref uint pvAttribute, int cbAttribute);
         /// <summary>
         /// https://learn.microsoft.com/en-us/windows/win32/api/dwmapi/ne-dwmapi-dwmwindowattribute
         /// </summary>
         const uint DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+        #endregion
+
+        #region CaptionColor
+        /// <summary>
+        /// Check whether Windows 11 build is 22000 or higher, that supports <see cref="SetCaptionColor(Window, Color)"/>.
+        /// </summary>
+        public static bool IsCaptionColorSupported { get; } =
+            Environment.OSVersion.Version.Build >= 22000;
+
+        /// <summary>
+        /// Sets the caption color for the specified window.
+        /// </summary>
+        /// <param name="window">The window for which to set the caption color.</param>
+        /// <param name="color">The color to set for the window caption.</param>
+        /// <returns>True if the caption color was set successfully; otherwise, false.</returns>
+        public static bool SetCaptionColor(Window window, Color color)
+        {
+            IntPtr hwnd = new System.Windows.Interop.WindowInteropHelper(window).Handle;
+            return SetCaptionColor(hwnd, color);
+        }
+
+        /// <summary>
+        /// Sets the caption color for the specified window.
+        /// </summary>
+        /// <param name="window">The window for which to set the caption color.</param>
+        /// <param name="colorRef">The color reference to set for the window caption. Defaults to DWMWA_COLOR_DEFAULT.</param>
+        /// <returns>True if the caption color was set successfully; otherwise, false.</returns>
+        public static bool SetCaptionColor(Window window, uint colorRef = DWMWA_COLOR_DEFAULT)
+        {
+            IntPtr hwnd = new System.Windows.Interop.WindowInteropHelper(window).Handle;
+            return SetCaptionColor(hwnd, colorRef);
+        }
+
+        /// <summary>
+        /// Sets the caption color for the specified window handle.
+        /// </summary>
+        /// <param name="hWnd">The handle of the window for which to set the caption color.</param>
+        /// <param name="color">The color to set for the window caption.</param>
+        /// <returns>True if the caption color was set successfully; otherwise, false.</returns>
+        public static bool SetCaptionColor(IntPtr hWnd, Color color)
+        {
+            var colorRef = (uint)ColorTranslator.ToWin32(color);
+            return SetCaptionColor(hWnd, colorRef);
+        }
+
+        /// <summary>
+        /// Sets the caption color for the specified window handle.
+        /// </summary>
+        /// <param name="hWnd">The handle of the window for which to set the caption color.</param>
+        /// <param name="colorRef">The color reference to set for the window caption. Defaults to DWMWA_COLOR_DEFAULT.</param>
+        /// <returns>True if the caption color was set successfully; otherwise, false.</returns>
+        public static bool SetCaptionColor(IntPtr hWnd, uint colorRef = DWMWA_COLOR_DEFAULT)
+        {
+            if (IsCaptionColorSupported == false)
+                return false;
+
+            var result = DwmSetWindowAttribute(hWnd, DWMWA_CAPTION_COLOR, ref colorRef, sizeof(int));
+            return result > 0;
+        }
+
+        /// <summary>
+        /// https://learn.microsoft.com/en-us/windows/win32/api/dwmapi/ne-dwmapi-dwmwindowattribute
+        /// </summary>
+        const uint DWMWA_CAPTION_COLOR = 35;
+        /// <summary>
+        /// Specifying DWMWA_COLOR_DEFAULT (value 0xFFFFFFFF) for the color will reset the window back to using the system's default behavior for the caption color.
+        /// </summary>
+        public const uint DWMWA_COLOR_DEFAULT = 0xffffffff;
         #endregion
     }
 }
--- RevitAddin.WindowDarkMode.Example/Views/SimpleView.xaml.cs
+++ RevitAddin.WindowDarkMode.Example/Views/SimpleView.xaml.cs
@@ -11,7 +11,6 @@ namespace RevitAddin.WindowDarkMode.Example.Views
             InitializeWindow();
 
             Title = $"Build: {Environment.OSVersion.Version.Build}";
-
             this.Loaded += (s, e) =>
             {
                 ChangeImmersiveDarkMode();
@@ -19,6 +18,25 @@ namespace RevitAddin.WindowDarkMode.Example.Views
             this.KeyDown += (s, e) =>
             {
                 if (e.Key == System.Windows.Input.Key.Escape) Close();
+                else if (e.Key == System.Windows.Input.Key.A) ChangeImmersiveDarkMode2();
+                else if (e.Key == System.Windows.Input.Key.S)
+                {
+                    DesktopWindowManager.SetCaptionColor(this, System.Drawing.Color.FromArgb(0x2e, 0x34, 0x40));
+                }
+                else if (e.Key == System.Windows.Input.Key.W)
+                {
+                    DesktopWindowManager.SetCaptionColor(this, System.Drawing.Color.FromArgb(0x3b, 0x44, 0x53));
+                }
+                else if (e.Key == System.Windows.Input.Key.Z)
+                    DesktopWindowManager.SetCaptionColor(this, System.Drawing.Color.Red);
+                else if (e.Key == System.Windows.Input.Key.X)
+                    DesktopWindowManager.SetCaptionColor(this, System.Drawing.Color.Blue);
+                else if (e.Key == System.Windows.Input.Key.C)
+                    DesktopWindowManager.SetCaptionColor(this, System.Drawing.Color.Green);
+                else if (e.Key == System.Windows.Input.Key.D)
+                {
+                    DesktopWindowManager.SetCaptionColor(this);
+                }
                 else ChangeImmersiveDarkMode();
             };
         }
@@ -34,14 +52,26 @@ namespace RevitAddin.WindowDarkMode.Example.Views
             var enableDarkMode = false;
             if (Background is System.Windows.Media.SolidColorBrush solidColor)
                 enableDarkMode = solidColor.Color == lightBackground.Color;
-            
+
             DesktopWindowManager.SetImmersiveDarkMode(this, enableDarkMode);
             Background = enableDarkMode ? darkBackground : lightBackground;
             Foreground = enableDarkMode ? darkForeground : lightForeground;
 
             //// Force to redraw the window border
-            WindowStyle = WindowStyle.None;
-            WindowStyle = WindowStyle.SingleBorderWindow;
+            //WindowStyle = WindowStyle.None;
+            //WindowStyle = WindowStyle.SingleBorderWindow;
+            this.ShowInTaskbar = !this.ShowInTaskbar;
+            this.ShowInTaskbar = !this.ShowInTaskbar;
+        }
+        bool enableDarkMode = false;
+        public void ChangeImmersiveDarkMode2()
+        {
+            enableDarkMode = !enableDarkMode;
+            DesktopWindowManager.SetImmersiveDarkMode(this, enableDarkMode);
+
+            //// Force to redraw the window border
+            this.ShowInTaskbar = !this.ShowInTaskbar;
+            this.ShowInTaskbar = !this.ShowInTaskbar;
         }
 
         #region InitializeWindow
